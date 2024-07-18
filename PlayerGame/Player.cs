63d9f4dbc5b14a77df8c439fa26167881ff0bffa
Sanskruti Itkar <139566431+Sanskrutiitkar@users.noreply.github.com>
2024-07-18 20:25:37 +0530

    Add files via upload

---
+++ PlayerGame/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerGame
+{
+    class Player
+    {
+        private int playerId;
+        private string playerName;
+        private int playerAge;
+
+        public Player(int id, string name) : this(id, name, 0)
+        {
+        }
+        public Player(int id, string name, int age)
+        {
+            this.playerId = id;
+            this.playerName = name;
+            this.playerAge = age;
+        }
+
+        public int GetId()
+        {
+            return playerId;
+        }
+
+        public string GetName()
+        {
+            return playerName;
+        }
+
+        public int GetAge()
+        {
+            return playerAge;
+        }
+        public static Player WhoIsElder(Player[] players)
+        {
+
+            Player elder = players[0];
+            for (int i = 1; i < players.Length; i++)
+            {
+                if (players[i].playerAge > elder.playerAge)
+                    elder = players[i];
+            }
+
+            return elder;
+        }
+    }
+}
---
+++ PlayerGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using PlayerGame;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        //Player player1 = new Player(1, "Seema", 25);
+        //Player player2 = new Player(2, "Rina", 30);
+        //Player player3 = new Player(3, "Sham");
+
+        Player[] players = new Player[3]
+        { new Player(1, "Seema", 25),
+        new Player(2, "Rina", 30),
+        new Player(3, "Sham")};
+
+    
+        Player elder = Player.WhoIsElder(players);
+
+        
+        Console.WriteLine($"Elder player: ID={elder.GetId()}, Name={elder.GetName()}, Age={elder.GetAge()}");
+    }
+}
